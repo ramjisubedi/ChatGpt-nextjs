89b9d09d2947ed647d0b23f36b37ad2c9094303a
ramji subedi <ramjisubedi1991@gmail.com>
2023-02-20 22:57:53 +0545

    creating home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="flex">
+          {/* Sidebar */}
+          {/* chat gpt notification thinking */}
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,73 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import { SunIcon, BoltIcon, ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col items-center justify-center px-2 h-screen text-white">
+        <h1 className="text-5xl font-bold mb-20">ChatGpt</h1>
+        <div className="flex space-x-4">
+          <div>
+          <div className="flex items-center justify-center flex-col mb-5">
+            {/* Icon */}
+            <div>
+              <SunIcon className="h-10 w-10 text-white" />
+            </div>
+            <h2>Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              "Explain quantum computing in simple terms"
+            </p>
+            <p className="infoText">
+              "Got any creative ideas for a 10 year old’s birthday?"
+            </p>
+            <p className="infoText">
+              "How do I make an HTTP request in Javascript?"
+            </p>
+          </div>
+          </div>
+          <div>
+          <div className="flex items-center justify-center flex-col mb-5">
+            {/* Icon */}
+            <div>
+              <BoltIcon className="h-10 w-10 text-white" />
+            </div>
+            <h2>Capabilities</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              "Remembers what user said earlier in the conversation"
+            </p>
+            <p className="infoText">
+              "Allows user to provide follow-up corrections"
+            </p>
+            <p className="infoText">
+              "Trained to decline inappropriate requests"
+            </p>
+          </div>
+          </div>
+          <div>
+          <div className="flex items-center justify-center flex-col mb-5">
+            {/* Icon */}
+            <div>
+              <ExclamationTriangleIcon className="h-10 w-10 text-white" />
+            </div>
+            <h2>Limitations</h2>
+          </div>
+          <div className="space-y-2 text-center">
+            <p className="infoText">
+              "May occasionally generate incorrect information"
+            </p>
+            <p className="infoText">
+              "May occasionally produce harmful instructions or biased content"
+            </p>
+            <p className="infoText">
+              "Limited knowledge of world and events after 2021"
+            </p>
+          </div>
+          </div>
         </div>
+        
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
