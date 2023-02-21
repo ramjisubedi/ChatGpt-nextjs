4ad5b0dffec9f5f12ed036c6863e237356802f30
ramji subedi <ramjisubedi1991@gmail.com>
2023-02-21 22:37:25 +0545

    onClick event for newChat

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { useRouter } from 'next/navigation';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useSession } from 'next-auth/react';
 
 const NewChat = () => {
+
+  const router = useRouter();
+  const {data:session} = useSession();
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),{
+        message:[],
+        userID:session?.user?.email!,
+        createdAt:serverTimestamp()
+      }
+    );
+    router.push(`/chat/${doc.id}`);
+  }
   return (
-    <div className='border-gray-700 border chatRow'>
+    <div onClick={createNewChat} className='border-gray-700 border chatRow'>
         <PlusIcon className='h-5 w-5'/>
         <span>New Chat</span>
     </div>
