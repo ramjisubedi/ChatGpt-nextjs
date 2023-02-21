d3b63a1d5b4a134ea332e5ab35e84933382d282c
ramji subedi <ramjisubedi1991@gmail.com>
2023-02-21 23:17:17 +0545

    get firebase data in newChat section

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,16 +1,26 @@
 'use client'
+import { db } from '../firebase';
+import { collection } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
+import {useCollection} from 'react-firebase-hooks/firestore'
 import NewChat from './NewChat'
+import ChatRow from './ChatRow';
 const SideBar = () => {
   const {data:session} = useSession();
+  const [chats, loading, error] = useCollection(
+    session && collection(db,'users',session?.user?.email!, "chats")
+  );
+  console.log(chats)
   return (
     <div className="flex flex-col h-screen p-2">
         <div className="flex-1">
             <div>
                 {/* New Chat */}
                 <NewChat />
-                {/* ModelSection */}
+                <div>{/* ModelSection */}</div>
 
+                {/* Map the ChatRow */}
+                {chats?.docs.map(chat => <ChatRow key={chat.id} id={chat.id}/>)}
             </div>
         </div>
         {session && (
