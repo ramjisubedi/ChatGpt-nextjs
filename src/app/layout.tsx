d817b07248fdead44f402c586870ce1598746d17
ramji subedi <ramjisubedi1991@gmail.com>
2023-02-21 09:03:08 +0545

    Next Auth Implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
+import { SessionProvider } from "../../components/SessionProvider";
 import "./globals.css";
-import SideBar from "components/sidebar";
-export default function RootLayout({
+import SideBar from "../../components/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <head />
       <body>
-        <div className="flex">
+        <SessionProvider session={session}>
+          {!session ? 'Not Login':
+          <div className="flex">
           <div className="bg-[#202123] max-w-xs h-screen overflow-y-scroll md:min-w-[20rem]">
             {/* Sidebar */}
             <SideBar />
@@ -17,7 +24,9 @@ export default function RootLayout({
 
           {/* chat gpt notification thinking */}
           <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+        </div>}
+        
+        </SessionProvider>
       </body>
     </html>
   );
